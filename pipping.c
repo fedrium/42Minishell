1be966156445544cf9a6f77d51bfce954e48ba4b
LeeYang <leeyang031110@gmail.com>
2023-09-02 19:26:11 +0800

    cat cat ls

--- pipping.c
+++ pipping.c
@@ -7,10 +7,10 @@ void    print_args(t_list *head_tokens)
 {
 	t_list *node;
 	node = head_tokens;
-	printf("tout: %s\n", ((t_token *)node->content)->token);
+	dprintf(2, "tout: %s\n", ((t_token *)node->content)->token);
 	while (node->next != NULL)
 	{
-		printf("tout: %s\n", ((t_token *)node->next->content)->token);
+		dprintf(2, "tout: %s\n", ((t_token *)node->next->content)->token);
 		if (node->next != NULL)
 			node = node->next;
 	}
@@ -36,96 +36,100 @@ void	print_cp(t_cp *head_cp)
 //run the functions in the child process
 // !!!need to add multiple pipes!!!
 
-t_cp	*make_cp(t_list *head_tokens)
+t_cp *make_cp(t_list *head_tokens, int *out, int *in)
 {
-	t_cp 	*cp_head;
-	t_cp	*cp_node;
-	int		ori_stdout;
-	int		ori_stdin;
+    t_cp *cp_head = NULL;
+    t_cp *cp_node = NULL;
+    int ori_stdout;
+    int ori_stdin;
 
-	dprintf(2, "error1\n");
-	cp_head = malloc(sizeof(t_cp));
-	dprintf(2, "error2\n");
-	if (cp_head == NULL)
-		printf("error3\n");
-	cp_head->next = NULL;
-	dprintf(2, "error4\n");
-	split_args(&cp_head->tokens, &head_tokens);
-	dprintf(2, "error5\n");
-	cp_node = cp_head;
-	// printf("first node\n");
-	// print_args(cp_node->tokens);
-	while (head_tokens != NULL)
-	{
-		dprintf(2, "error6\n");
-		cp_node->next = malloc(sizeof(t_cp));
-		if (cp_node->next == NULL)
-			printf("error7\n");
-		cp_node->next->next = NULL;
-		split_args(&cp_node->next->tokens, &head_tokens);
-		dprintf(2, "error8\n");
-		cp_node = cp_node->next;
-		dprintf(2, "error9\n");
-	}
-	dprintf(2, "error10\n");
-	cp_node = cp_head;
-	// print_cp(cp_head);
-	ori_stdout = dup(STDOUT_FILENO);
-	ori_stdin = dup(STDIN_FILENO);
-	//when pipe runs, tokenize run twice, why?
-	pipe(cp_node->pipe);
-	dup2(cp_node->pipe[0], STDIN_FILENO);
-	dup2(cp_node->pipe[1], STDOUT_FILENO);
-	close(cp_node->pipe[0]);
-	close(cp_node->pipe[1]);
-	while (cp_node->next != NULL)
-	{
-		dprintf(2, "erro11r\n");
-		// pipe(cp_node->pipe);
-		dprintf(2, "error12\n");
-		pipe(cp_node->next->pipe);
-		dprintf(2, "error13\n");
-		dup2(cp_node->next->pipe[1], cp_node->pipe[1]);
-		dprintf(2, "error14\n");
-		close(cp_node->next->pipe[1]);
-		dprintf(2, "error15\n");
-		// segfault here
-		// close(cp_node->next->next->pipe[0]);
-		dprintf(2, "error16\n");
-		cp_node = cp_node->next;
-		dprintf(2, "error17\n");
-	}
-	dup2(ori_stdout, STDOUT_FILENO);
-	close(ori_stdout);
-	dup2(ori_stdin, STDIN_FILENO);
-	close(ori_stdin);
-	close(cp_node->pipe[0]);
-	close(cp_node->pipe[1]);
-	dprintf(2, "error 18\n");
-	return (cp_head);
+    cp_head = malloc(sizeof(t_cp));
+    // if (cp_head == NULL)
+    // {
+    //     perror("malloc");
+    //     exit(EXIT_FAILURE);
+    // }
+    cp_head->next = NULL;
+    split_args(&cp_head->tokens, &head_tokens);
+    cp_node = cp_head;
+    while (head_tokens != NULL)
+    {
+        cp_node->next = malloc(sizeof(t_cp));
+        // if (cp_node->next == NULL)
+        // {
+        //     perror("malloc");
+        //     exit(EXIT_FAILURE);
+        // }
+        cp_node->next->next = NULL;
+        split_args(&cp_node->next->tokens, &head_tokens);
+		// print_cp(cp_node);
+        cp_node = cp_node->next;
+    }
+    ori_stdout = dup(STDOUT_FILENO);
+    ori_stdin = dup(STDIN_FILENO);
+    cp_node = cp_head;
+    while (cp_node != NULL)
+    {
+        pipe(cp_node->pipe);
+        cp_node = cp_node->next;
+    }
+    (*out) = ori_stdout;
+	(*in) = ori_stdin;
+    return (cp_head);
 }
 
-void     organise_args(t_list *head_tokens, t_list *head_env)
+
+void organise_args(t_list *head_tokens, t_list *head_env) 
 {
-	t_cp	*child_processes;
-	pid_t	pid;
+    t_cp *child_processes;
+    pid_t pid;
+    int ori_stdout;
+    int ori_stdin;
+    int num_processes;
 	
-	pid = 1;
-	child_processes = make_cp(head_tokens);
-	// dprintf(2, "error bitch!\n");
-	while (child_processes != NULL)
+	num_processes = 0; // Count the number of child processes
+    pid = 1;
+    child_processes = make_cp(head_tokens, &ori_stdout, &ori_stdin);
+    while (child_processes != NULL) 
+    {
+        num_processes++; // Increment the number of child processes
+        pid = fork();
+        if (pid == 0) 
+        {
+            // Redirect STDIN to read from its own pipe
+            dup2(child_processes->pipe[0], STDIN_FILENO);
+            close(child_processes->pipe[0]); // Close read end of its own pipe
+            if (child_processes->next == NULL) 
+            {
+                // Redirect STDOUT to the original STDOUT
+                dup2(ori_stdout, STDOUT_FILENO);
+                close(ori_stdout); // Close the original STDOUT
+            } 
+            else 
+            {
+                // Redirect STDOUT to write to the pipe of the next child process
+                dup2(child_processes->next->pipe[1], STDOUT_FILENO);
+                close(child_processes->next->pipe[1]); // Close write end of the next pipe
+            }
+            // Don't close STDIN and STDOUT here
+			// close(child_processes->pipe[0]);
+            close(child_processes->pipe[1]);
+            run_functions(child_processes->tokens, head_env);
+            exit(0);
+        } 
+        else 
+        {
+            close(child_processes->pipe[1]);
+            child_processes = child_processes->next;
+        }
+    }
+    // Wait for all child processes to exit
+	while (num_processes > 0)
 	{
-		pid = fork();
-		if (pid == 0)
-		{
-			run_functions(child_processes->tokens, head_env);
-			dprintf(2, "error 19\n");
-			exit(0);
-		}
-		else
-			child_processes = child_processes->next;
+		wait(NULL);
+		num_processes--;
 	}
-	while (wait(NULL) > 0);
+	// wait(NULL);
 }
 
 //return head of the start of the command, point last argument to NULL instead of pipe
